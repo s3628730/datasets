400461875c1fa8c4f2103cfaafee6e70127b23b4
Dr. Vural Aksakalli <vaksakalli@gmail.com>
2019-06-12 10:37:33 +1000

    Update prepare_dataset_for_modeling.py

--- prepare_dataset_for_modeling.py
+++ prepare_dataset_for_modeling.py
@@ -4,15 +4,17 @@ import io
 import requests
 import ssl
 from sklearn import preprocessing
+from sklearn.utils import shuffle
+
 
 def prepare_dataset_for_modeling(dataset_name, n_obs=-1):
     """
-    :param dataset_name: name of dataset to be read from the github
+    :param dataset_name: name of dataset to be read from github
     :param n_obs: how many observations to sample (if > 0)
-    :return: clean x and y NumPy arrays that are ready for model fitting
+    :return: x and y NumPy arrays ready for model fitting
     """
 
-    # read in the CSV file in to a Pandas data frame
+    # read in the CSV file into a Pandas data frame
     if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
             getattr(ssl, '_create_unverified_context', None)):
         ssl._create_default_https_context = ssl._create_unverified_context
@@ -27,29 +29,41 @@ def prepare_dataset_for_modeling(dataset_name, n_obs=-1):
     if n_obs > 0:
         df = df.sample(n=n_obs, replace=False, random_state=123)
 
-    y = df.iloc[:, -1]  # last column is y (target feature)
-    x = df.iloc[:, :-1]  # everything else is x (set of descriptive features)
+    # drop ID-like columns
+    df = df.loc[:, df.nunique() < df.shape[0]]
+    # drop constant columns
+    df = df.loc[:, df.nunique() > 1]
+
+    # last column is y (target feature)
+    y = df.iloc[:, -1]
+    # everything else is x (set of descriptive features)
+    x = df.iloc[:, :-1]
 
     # get all columns that are strings
     # these are assumed to be nominal categorical
     categorical_cols = x.columns[x.dtypes == object].tolist()
 
-    # if a nominal feature has only 2 levels
-    # define a single binary variable for it
+    # if a nominal feature has only 2 levels:
+    # encode it as a single binary variable
     for col in categorical_cols:
         n = len(x[col].unique())
         if n == 2:
             x[col] = pd.get_dummies(x[col], drop_first=True)
 
-    # use one-hot-encoding for categorical features with >2 levels
+    # for categorical features with >2 levels: use one-hot-encoding
+    # below, numerical columns will be untouched
     x = pd.get_dummies(x)
 
     # scale x between 0 and 1
     x = preprocessing.MinMaxScaler().fit_transform(x)
     # label-encode y
     y = preprocessing.LabelEncoder().fit_transform(y)
+    # shuffle data at the end
+    x, y = shuffle(x, y)
 
     return x, y
 
-# x, y = prepare_dataset_for_modeling('us_census_income_data.csv')
+
+# ## how to run this script
+x, y = prepare_dataset_for_modeling('us_census_income_data.csv')
 
